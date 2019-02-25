1fd358969892c0b471fb9c1a944a576ff485fe70
Valeria Dopira <leradopira@mail.ru>
2019-02-25 23:41:05 +0300

    initial

---
+++ 1.py
@@ -0,0 +1,33 @@
+import collections
+
+def main():
+    directory = '/home/valerie/University/Статистика/labs/Dopira.csv'
+
+    with open(directory, 'r') as file: #read from file
+        file1 = file.readlines()
+    print('Данные из файла: ',file1)
+    print('Размер выборки: ', len(file1)-1)
+
+    sequence = list()
+    for i in range(1, len(file1)): #make up the initial sequence
+        sequence.insert(i, float(file1[i].split(',')[0]))
+    print('Изначальный ряд: ', sequence)
+
+    rangeSequence = sorted(sequence)
+    print('Ранжированный ряд: ', rangeSequence)
+
+    variation = list()
+    count = collections.Counter()
+    for i in rangeSequence:
+        count[i] += 1
+
+    for key, value in count.items():
+        variation.insert(0, [key,value])
+    variationSequence = sorted(variation)
+    print('Вариационный ряд: ',variationSequence)
+
+
+
+
+if __name__ == "__main__":
+    main()
