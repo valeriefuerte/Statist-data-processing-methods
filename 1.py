fde59b7117b1f835230c3fd4267390c5bf9ee92b
Valeria Dopira <leradopira@mail.ru>
2019-02-26 00:11:48 +0300

    Sturges’ rule

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import collections
+import math
 
 def main():
     directory = '/home/valerie/University/Статистика/labs/Dopira.csv'
@@ -6,7 +7,7 @@ def main():
     with open(directory, 'r') as file: #read from file
         file1 = file.readlines()
     print('Данные из файла: ',file1)
-    print('Размер выборки: ', len(file1)-1)
+    #print('Размер выборки: ', len(file1)-1)
 
     sequence = list()
     for i in range(1, len(file1)): #make up the initial sequence
@@ -29,6 +30,15 @@ def main():
     with open('variation.csv', 'w') as variationFile:
             for i in range(len(variationSequence)):
                 variationFile.write(str(variationSequence[i][0]) + ';' + str(variationSequence[i][1]) + '\n')
+
+    n = 0
+    for i in range(len(variationSequence)):
+        n = variationSequence[i][1] + n
+    print('Объем выборки', n)
+
+    K = int(1 + 3.322 * math.log(n, 10))
+    print('Количество интервалов', K)
+
 
 if __name__ == "__main__":
     main()
